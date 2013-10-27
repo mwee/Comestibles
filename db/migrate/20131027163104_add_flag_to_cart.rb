ff0c7768f60c44036eb9e10059040341b5c1e548
Vladimir <vladimirbok@college.harvard.edu>
2013-10-27 19:20:16 -0400

    clen once again

--- db/migrate/20131027163104_add_flag_to_cart.rb
+++
@@ -1,9 +0,0 @@
-class AddFlagToCart < ActiveRecord::Migration
-  def up
-    add_column :carts, :finalized, :boolean, default: false
-  end
-
-  def down
-    remove_column :carts, :finalized
-  end
-end
--- db/migrate/20131027194428_add_name_to_carts.rb
+++
@@ -1,6 +0,0 @@
-class AddNameToCarts < ActiveRecord::Migration
-  def change
-    add_column :carts, :cart_name, :string
-
-  end
-end
