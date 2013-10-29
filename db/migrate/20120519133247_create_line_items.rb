2b6e38ebfe8aac83914ead30bc5752a7e133d9df
Michael Wee <theweester@gmail.com>
2013-10-28 20:31:56 -0400

    fixed line_item decimal/integer bug

--- db/migrate/20120519133247_create_line_items.rb
+++ db/migrate/20120519133247_create_line_items.rb
@@ -2,7 +2,7 @@ class CreateLineItems < ActiveRecord::Migration
   def change
     create_table :line_items do |t|
       t.decimal :unit_price
-      t.decimal :product_id
+      t.integer :product_id
       t.integer :cart_id
       t.integer :quantity
 
